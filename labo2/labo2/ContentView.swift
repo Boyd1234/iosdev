0342dfeb901e325d5ee683da8ba7a53c91f3e596
Boyd Bulcaen <boyd@iMacC.local>
2025-10-01 10:23:32 +0200

    labo 2 volledig klaar

--- labo2/labo2/ContentView.swift
+++ labo2/labo2/ContentView.swift
@@ -8,22 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    let flightToBarcelona = FlightInfo(
+        shortcodeDepart: "BRU",
+        shortcodeArrival: "BCN",
+        stationDepart: "Brussels",
+        stationArrival: "Barcelona",
+        departTime: "8:15",
+        arrivalTime: "11:15",
+        flightID: "SN23A",
+        gate: "B23",
+        seat: "27A",
+        passenger: "Dirk Hostens",
+        klasse: "Business",
+        flightDate: "27/08/2024"
+    )
+
+    let flightBack = FlightInfo(
+        shortcodeDepart: "BCN",
+        shortcodeArrival: "BRU",
+        stationDepart: "Barcelona",
+        stationArrival: "Brussels",
+        departTime: "13:05",
+        arrivalTime: "15:15",
+        flightID: "SN205",
+        gate: "XD-30",
+        seat: "17C",
+        passenger: "Dirk Hostens",
+        klasse: "Business",
+        flightDate: "27/08/2024"
+    )
+
     var body: some View {
-        TabView{
-            
+        TabView {
             Tab("departure", systemImage: "airplane.departure") {
-                DepartureView(shortcodeDepart: "BRU", shortcodeArrival: "BCN", stationDepart: "Brussels", stationArrival: "Barcelona", departTime: "8:15", arrivalTime: "11:15", flightID: "SN23A", gate: "B23", seat: "27A", passenger: "Dirk Hostens", klasse: "Business", flightDate: "27/08/2024")
+                DepartureView(info: flightToBarcelona)
             }
+
             Tab("info", systemImage: "info") {
                 InfoView()
             }
-            
-            Tab("Return", systemImage: "airplane.arrival") {
-                DepartureView(shortcodeDepart: "BCN", shortcodeArrival: "BRU", stationDepart: "Barcelona", stationArrival: "Brussels", departTime: "13:05", arrivalTime: "15:15", flightID: "SN205", gate: "XD-30", seat: "17C", passenger: "Dirk Hostens", klasse: "Business", flightDate: "27/08/2024")
+
+            Tab("return", systemImage: "airplane.arrival") {
+                DepartureView(info: flightBack)
             }
         }
     }
 }
+
 
 #Preview {
     ContentView()
--- labo2/labo2/DepartureView.swift
+++ labo2/labo2/DepartureView.swift
@@ -7,96 +7,117 @@
 
 import SwiftUI
 
+struct FlightInfo {
+    let shortcodeDepart: String
+    let shortcodeArrival: String
+    let stationDepart: String
+    let stationArrival: String
+    let departTime: String
+    let arrivalTime: String
+    let flightID: String
+    let gate: String
+    let seat: String
+    let passenger: String
+    let klasse: String
+    let flightDate: String
+}
+
 struct DepartureView: View {
-    var shortcodeDepart : String
-    var shortcodeArrival : String
-    var stationDepart : String
-    var stationArrival : String
-    var departTime : String
-    var arrivalTime : String
-    var flightID : String
-    var gate : String
-    var seat : String
-    var passenger : String
-    var klasse : String
-    var flightDate : String
+    let info : FlightInfo
+    
     var body: some View {
-        Grid {
-            GridRow{
-                Text(shortcodeDepart)
-                    .font(.system(size: 36))
-                    .bold()
-                    .foregroundColor(.green)
-                Text("")
-                Text(shortcodeArrival)
-                    .font(.system(size: 36))
-                    .bold()
-                    .foregroundColor(.green)
-                
-            }
-            GridRow{
-                Text(stationDepart).foregroundColor(.gray)
-                Image(systemName: "airplane").font(.system(size: 65))
-                Text(stationArrival).foregroundColor(.gray)
-            }
-            GridRow{
-                Text(departTime)
-                Text("")
-                Text(arrivalTime)
-            }
-            GridRow{
-                Text("")
-            }
-
-        
-            GridRow {
-                VStack {
-                    HStack {
-                        Text("flight")
-                        Spacer()
-                        Text("gate")
-                        Spacer()
-                        Text("seat")
-                    }
-                    .foregroundColor(.white)
-                    .font(.subheadline)
-
-                    HStack {
-                        Text(flightID).bold()
-                        Spacer()
-                        Text(gate).bold()
-                        Spacer()
-                        Text(seat).bold()
-                    }
-                    .foregroundColor(.white)
-                    .font(.title3)
+        VStack(spacing: 20) {
+            Grid {
+                GridRow {
+                    Text(info.shortcodeDepart)
+                        .font(.system(size: 36))
+                        .bold()
+                        .foregroundColor(.green)
+                    Spacer()
+                    Text(info.shortcodeArrival)
+                        .font(.system(size: 36))
+                        .bold()
+                        .foregroundColor(.green)
                 }
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(Color.green)
-                .cornerRadius(10)
-                .gridCellColumns(3) // Laat deze GridRow over alle 3 kolommen gaan
-            }
+                GridRow {
+                    Text(info.stationDepart).foregroundColor(.gray)
+                    Image(systemName: "airplane").font(.system(size: 65))
+                    Text(info.stationArrival).foregroundColor(.gray)
+                }
+                GridRow {
+                    Text(info.departTime)
+                    Spacer()
+                    Text(info.arrivalTime)
+                }
+                GridRow {
+                    Spacer()
+                }
+                GridRow {
+                    VStack {
+                        HStack {
+                            Text("flight")
+                            Spacer()
+                            Text("gate")
+                            Spacer()
+                            Text("seat")
+                        }
+                        .foregroundColor(.white)
+                        .font(.subheadline)
 
+                        HStack {
+                            Text(info.flightID).bold()
+                            Spacer()
+                            Text(info.gate).bold()
+                            Spacer()
+                            Text(info.seat).bold()
+                        }
+                        .foregroundColor(.white)
+                        .font(.title3)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    .gridCellColumns(3)
+                }
             }
-        HStack{
-            VStack{
-                Text("Passenger").foregroundColor(.gray)
-                Text(passenger)
-                Text("")
-                Text("Class").foregroundColor(.gray)
-                Text(klasse)
-                Text("")
-                Text("Flight date")
-                Text(flightDate)
+            .padding()
+
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Passenger").foregroundColor(.gray)
+                    Text(info.passenger)
+                    Text("")
+                    Text("Class").foregroundColor(.gray)
+                    Text(info.klasse)
+                    Text("")
+                    Text("Flight date")
+                    Text(info.flightDate)
+                }
+                Spacer()
+                Image(systemName: "person.crop.artframe")
+                    .font(.system(size: 180))
+                    .foregroundColor(.gray)
             }
-            Image(systemName: "person.crop.artframe")
-                .font(.system(size: 180))
-                .foregroundColor(.gray)
+            .padding()
         }
-        }
+        .padding()
+    }
 }
 
 #Preview {
-    DepartureView(shortcodeDepart: "BRU", shortcodeArrival: "BCN", stationDepart: "Brussels", stationArrival: "Barcelona", departTime: "8:15", arrivalTime: "11:15", flightID: "SN23A", gate: "B23", seat: "27A", passenger: "Dirk Hostens", klasse: "Business", flightDate: "27/08/2024")
+    DepartureView(info: FlightInfo(
+        shortcodeDepart: "BRU",
+        shortcodeArrival: "BCN",
+        stationDepart: "Brussels",
+        stationArrival: "Barcelona",
+        departTime: "8:15",
+        arrivalTime: "11:15",
+        flightID: "SN23A",
+        gate: "B23",
+        seat: "27A",
+        passenger: "Dirk Hostens",
+        klasse: "Business",
+        flightDate: "27/08/2024"
+    ))
 }
