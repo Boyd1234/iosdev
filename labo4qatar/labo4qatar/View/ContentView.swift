3d9cd40f1f49c9c022861ee652c08abbd86f8d04
Boyd Bulcaen <boyd@iMacC.local>
2025-10-15 10:43:38 +0200

    lab4 demo2 section4 klaar

---
+++ labo4qatar/labo4qatar/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  labo4qatar
+//
+//  Created by Boyd Bulcaen on 15/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ labo4qatar/labo4qatar/labo4qatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  labo4qatarApp.swift
+//  labo4qatar
+//
+//  Created by Boyd Bulcaen on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct labo4qatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
